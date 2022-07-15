ea468f760d4f0d78d1b34f5e8983b97e3a13ed66
Javier Miranda <javi@Javiers-Air.attlocal.net>
2022-07-15 00:32:02 -0700

    added main file and venv

---
+++ main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+import tkinter as tk
+# useful links
+#   https://tkdocs.com/
+#   https://docs.python.org/3/library/tk.html
+# create the window and give it a title...
+window = Tk()
+window.title("Welcome to number guess")
+
+#set the window size
+window.geometry('900x150')
+
+window.tk.call('tk','scaling',3.0)
+
+window.mainloop()
+
+
+
