6aa2c541fe532c64b4c5b633800494f4000395b1
Javier Miranda <javi@Javiers-Air.attlocal.net>
2022-07-27 22:48:27 -0700

    understanding how test code works and making edits to see functionality

--- tkTutorial.py
+++ tkTutorial.py
@@ -2,26 +2,26 @@
 #learning how to use tkinter and best practices
 from tkinter import *
 from tkinter import ttk
-
+# function for feet to meters conversion
 def calculate(*args):
     try:
         value = float(feet.get())
         meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
     except ValueError:
         pass
-
+# Setting up the Main Application Window
 root = Tk()
-root.title("Feet to Meters")
-
+root.title("Feet to Meters Converter")
+# creating a content frame
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
-
+# Creating the Entry Widget
 feet = StringVar()
 feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
 feet_entry.grid(column=2, row=1, sticky=(W, E))
-
+# Creating the Remaining Widgets
 meters = StringVar()
 ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
 
@@ -30,11 +30,11 @@ ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3,
 ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
 ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
 ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
+# Adding Some Polish
 for child in mainframe.winfo_children(): 
     child.grid_configure(padx=5, pady=5)
 
 feet_entry.focus()
 root.bind("<Return>", calculate)
-
+# Start the Event Loop
 root.mainloop()
