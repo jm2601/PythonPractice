4fd7343decb03de81e8ae40b8eba4505a3a3766f
Javier Miranda <javi@Javiers-Air.attlocal.net>
2022-08-01 06:48:32 -0700

    creating print maze function

--- intermediate_projects/algorithmVisual.py
+++ intermediate_projects/algorithmVisual.py
@@ -16,12 +16,20 @@ maze = [
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
 
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color.pair(1)
+    RED = curses.color.pair(2)
+
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            stdscr.addstr(i, j*2, value, BLUE)
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    stdscr.clear()
-    stdscr.addstr(5,5, "hello world!")
-    stdscr.refresh()
-    stdscr.getch()
+    # stdscr.clear()
+    # stdscr.addstr(5,5, "hello world!")
+    # stdscr.refresh()
+    # stdscr.getch()
 
 wrapper(main)
