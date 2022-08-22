4975065b79f35db36627f2577262475f87751be6
Javier <javier.miranda4420@gmail.com>
2022-08-21 20:41:57 -0700

    finalized stringify_list() to print linked list

--- data_structures/linked_lists.py
+++ data_structures/linked_lists.py
@@ -3,20 +3,24 @@ class Node:
   def __init__(self, value, next_node=None):
     self.value = value
     self.next_node = next_node
+
   def get_value(self):
     return self.value
+
   def get_next_node(self):
     return self.next_node
+
   def set_next_node(self, next_node):
     self.next_node = next_node
 
-my_node = Node(44)
-print(my_node.get_value())
+# my_node = Node(44)
+# print(my_node.get_value())
 
 # Create your LinkedList class below:
 class LinkedList:
   def __init__(self, value=None):
     self.head_node = Node(value)
+
   def get_head_node(self):
     return self.head_node
 
@@ -34,8 +38,8 @@ class LinkedList:
     while current_node:
       if current_node.get_value() != None:
         string_list += str(current_node.get_value()) + "\n"
-        current_node = current_node.get_next_node()
-      return string_list
+      current_node = current_node.get_next_node()
+    return string_list
   
 
 # Test your code by uncommenting the statements below - did your list print to the terminal?
